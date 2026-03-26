ea0b4825146d05d68be6acb90ada1ed60f36ab49
harikumar1433 <harikumar1433@gmail.com>
2026-03-26 17:39:57 +0530

    added week6 day4

---
+++ Week_6/Day-4/Problem1/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+  
+        List<Product> products = new List<Product>()
+        {
+             new Product { Id = 1, Name = "Laptop", Price = 50000, Category = "Electronics" },
+             new Product { Id = 2, Name = "Mobile", Price = 20000, Category = "Electronics" },
+             new Product { Id = 3, Name = "Shoes", Price = 3000, Category = "Fashion" }
+       };
+
+      
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+       
+        public IActionResult Details(int id)
+        {
+           
+            Product product = products.FirstOrDefault(products => products.Id == id);
+
+            return View(product);
+        }
+    }
+}

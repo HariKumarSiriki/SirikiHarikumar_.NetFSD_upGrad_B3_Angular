b6c1a8789c131a43706e3222ea6e19d88710fc73
harikumar1433 <harikumar1433@gmail.com>
2026-03-29 10:05:06 +0530

    added week6 day5

---
+++ Week_6/Day_5/problem1/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [StringLength(15, MinimumLength = 5)]
+        public string Category { get; set; }
+
+    }
+}
---
+++ Week_6/Day_5/problem1/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+  
+      static  List<Product> products = new List<Product>()
+        {
+             new Product { Id = 1, Name = "Laptop", Price = 50000, Category = "Electronics" },
+             new Product { Id = 2, Name = "Mobile", Price = 20000, Category = "Electronics" },
+             new Product { Id = 3, Name = "Shoes", Price = 3000, Category = "Fashion" }
+       };
+
+      
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+        public IActionResult Details(int id)
+        {
+            Product prodObj = products.FirstOrDefault(item => item.Id == id);
+            return View(prodObj);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                products.Add(p);
+                return RedirectToAction("Index");
+            }
+
+            return View(p);
+        }
+
+
+
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product p)
+        {
+            var product = products.FirstOrDefault(x => x.Id == p.Id);
+
+            if (product != null)
+            {
+                product.Name = p.Name;
+                product.Price = p.Price;
+                product.Category = p.Category;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product != null)
+            {
+                products.Remove(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}

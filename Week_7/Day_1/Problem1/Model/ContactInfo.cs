9c10ccba3dd3534b4e96a49d4fc10e6a182393be
harikumar1433 <harikumar1433@gmail.com>
2026-03-30 17:26:01 +0530

    added week7 day1 files

---
+++ Week_7/Day_1/Problem1/Model/ContactInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class ContactInfo
+    {
+        [Required]
+        public int ContactId { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailId { get; set; }
+
+        [Required]
+        [Range(6000000000, 9999999999)]
+        public long MobileNo { get; set; }
+
+        [Required]
+        public string Designation { get; set; }
+
+    }
+}

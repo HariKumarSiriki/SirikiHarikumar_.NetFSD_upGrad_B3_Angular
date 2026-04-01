714538b0a8158d78797b40f27ee59fc9cab029b5
harikumar1433 <harikumar1433@gmail.com>
2026-04-01 18:55:01 +0530

    added week7 day3

---
+++ Week_7/Day_3/Problem1/Controller/MoviesController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Models.Data;
+
+namespace WebApplication1.Controllers
+{
+    
+
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public IActionResult Index()
+        {
+            var movies = _context.Movies.ToList();
+            return View(movies);
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+      
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(movie);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        
+        public IActionResult Edit(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            return View(movie);
+        }
+
+        
+        [HttpPost]
+        public IActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Update(movie);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+     
+        public IActionResult Delete(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            return View(movie);
+        }
+    }
+}
---
+++ Week_7/Day_3/Problem2/Controller/ContactController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Service;
+
+
+namespace WebApplication1.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+        public IActionResult ShowContacts()
+        {
+            var contacts = _contactService.GetAllContacts();
+            return View(contacts);
+        }
+        public IActionResult GetContactById(int id)
+        {
+            var contact = _contactService.GetContactById(id);
+            if (contact == null) return NotFound();
+
+            return View(contact);
+        }
+
+
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddContact(ContactInfo contactInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _contactService.AddContact(contactInfo);
+                return RedirectToAction("ShowContacts");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Data";
+                return View();
+            }
+        }
+    }
+}

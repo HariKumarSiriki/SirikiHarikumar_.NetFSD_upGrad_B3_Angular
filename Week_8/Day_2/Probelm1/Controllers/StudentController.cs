7240e4e79fe5ffce71187d2132742ac35947471c
harikumar1433 <harikumar1433@gmail.com>
2026-04-07 18:02:06 +0530

    added week8 day2

---
+++ Week_8/Day_2/Probelm1/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Repository;
+
+namespace WebApplication5.Controllers
+{
+    public class StudentController : Controller
+    {
+      
+
+
+        private readonly IStudentRepository _repo;
+
+        public StudentController(IStudentRepository repo)
+        {
+            _repo = repo;
+        }
+
+
+        public IActionResult Students()
+        {
+            var data = _repo.GetStudentsWithCourse();
+            return View(data);
+        }
+
+
+        public IActionResult Courses()
+        {
+            var data = _repo.GetCoursesWithStudents();
+            return View(data);
+        }
+    }
+}
---
+++ Week_8/Day_2/Probelm1/Repository/IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication5.Models;
+
+namespace WebApplication5.Repository
+{
+    public interface IStudentRepository
+    {
+         IEnumerable<Student> GetStudentsWithCourse();
+      IEnumerable<Course> GetCoursesWithStudents();
+    }
+}

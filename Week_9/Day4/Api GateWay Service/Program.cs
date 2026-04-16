ccde2e58b7b9a13dd4ce1b12f2f5755fbd8f6288
harikumar1433 <harikumar1433@gmail.com>
2026-04-16 21:13:16 +0530

    added weel9 day4

---
+++ Week_9/Day4/Api GateWay Service/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Text;
+
+namespace Api_GateWay_Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration
+     .SetBasePath(builder.Environment.ContentRootPath)
+     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+     .AddEnvironmentVariables();
+
+          
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+           .AddJwtBearer(options =>
+           {
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+                   ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                   ValidAudience = builder.Configuration["Jwt:Audience"],
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+               };
+           });
+
+            builder.Services.AddOcelot(builder.Configuration);
+            var app = builder.Build();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseOcelot().Wait();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
